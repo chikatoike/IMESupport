3cdb5acabc422153a885e8859489164bdfe1d0b2
chikatoike <chikatoike@gmail.com>
2012-10-23 13:57:33 +0900

    Set font size (not work)

--- imesupportplugin.py
+++ imesupportplugin.py
@@ -18,9 +18,11 @@ from imesupport import subclass
 # {'cells': [[0L, 0L, 1L, 1L], [1L, 0L, 2L, 1L], [2L, 0L, 3L, 1L]], 'rows': [0.0, 1.0], 'cols': [0.0, 0.33000000000000002, 0.66000000000000003, 1.0]}
 
 
+import ctypes
 from ctypes import windll, byref
-from ctypes.wintypes import RECT, POINT
 from ctypes import Structure, c_ulong
+from ctypes.wintypes import RECT, POINT
+from ctypes.wintypes import BYTE, LONG
 
 WM_IME_STARTCOMPOSITION = 0x10D
 
@@ -39,13 +41,47 @@ def mul(pos, factor):
 
 class COMPOSITIONFORM(Structure):
     _fields_ = [
-        ("dwStyle", c_ulong),
-        ("ptCurrentPos", POINT),
-        ("rcArea", RECT),
+        ('dwStyle', c_ulong),
+        ('ptCurrentPos', POINT),
+        ('rcArea', RECT),
     ]
 
 
-def set_inline_position(hwnd, x, y):
+# from http://d.hatena.ne.jp/pipehead/20071210
+import sys
+
+(major, platform) = sys.getwindowsversion()[0:4:3]
+winNT5OrLater = (platform == 2) and (major >= 5)
+LF_FACESIZE = 32
+
+
+class c_tchar(ctypes._SimpleCData):
+    if winNT5OrLater:
+        _type_ = 'u'  # c_wchar
+    else:
+        _type_ = 'c'  # c_char
+
+
+class LOGFONT(Structure):
+    _fields_ = [
+        ('lfHeight',         LONG),
+        ('lfWidth',          LONG),
+        ('lfEscapement',     LONG),
+        ('lfOrientation',    LONG),
+        ('lfWeight',         LONG),
+        ('lfItalic',         BYTE),
+        ('lfUnderline',      BYTE),
+        ('lfStrikeOut',      BYTE),
+        ('lfCharSet',        BYTE),
+        ('lfOutPrecision',   BYTE),
+        ('lfClipPrecision',  BYTE),
+        ('lfQuality',        BYTE),
+        ('lfPitchAndFamily', BYTE),
+        ('lfFaceName',       c_tchar * LF_FACESIZE)
+    ]
+
+
+def set_inline_position(hwnd, x, y, font_face, font_size):
     # borrowed from http://d.hatena.ne.jp/doloopwhile/20090627/1275176169
     hIMC = windll.imm32.ImmGetContext(hwnd)
     status = windll.imm32.ImmGetOpenStatus(hIMC)
@@ -58,6 +94,13 @@ def set_inline_position(hwnd, x, y):
     cf.dwStyle = 2      # CFS_POINT
     cf.ptCurrentPos = pt
     windll.imm32.ImmSetCompositionWindow(hIMC, byref(cf))
+
+    # AttributeError: function 'ImmSetCompositionFont' not found
+    # lf = LOGFONT()
+    # lf.lfHeight = font_size
+    # lf.lfFaceName = font_face
+    # windll.imm32.ImmSetCompositionFont(hIMC, byref(lf))
+
     windll.imm32.ImmReleaseContext(hwnd, hIMC)
 
 
@@ -73,30 +116,31 @@ def calc_position(view):
     # sublime.status_message(str(p))
 
     offset = (
-        get_setting("sublimeimeinline_view_offset_x", 30),
-        get_setting("sublimeimeinline_view_offset_y", 50))
+        get_setting('imesupport_view_offset_x', 44),
+        get_setting('imesupport_view_offset_y', 36))
+    font_face = view.settings().get('font_face', 10)
+    font_size = int(view.settings().get('font_size', 10))
 
     p = add(p, offset)
-    # sublime.status_message(str(p))
-    return (int(p[0]), int(p[1]))
+    sublime.status_message(str(p))
+    return (int(p[0]), int(p[1]), font_face, font_size)
 
 
 def get_setting(key, default=None):
-    try:
-        s = sublime.load_settings("IMESupport.sublime-settings")
-        return s.get(key, default)
-    except:
-        return default
+    return sublime.load_settings('IMESupport.sublime-settings').get(key, default)
 
 
 last_pos = (0, 0)
 
 def callback(hwnd, msg, wParam, lParam):
     if msg == WM_IME_STARTCOMPOSITION:
-        set_inline_position(hwnd, last_pos[0], last_pos[1])
-        import os
-        with open(os.path.expandvars('$HOME/log.txt'), 'a') as f:
-            f.write('last_pos: ' + str(last_pos) + '\n')
+        try:
+            set_inline_position(hwnd, *last_pos)
+        except Exception, e:
+            import os
+            with open(os.path.expandvars('$HOME/log.txt'), 'a') as f:
+                f.write('last_pos: ' + str(last_pos) + '\n')
+                f.write('Exception: ' + str(e) + '\n')
     return None
 
 
